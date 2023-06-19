29d65b5874d019317b4b94bb13edf49e9520a9c5
Errytagedesign <errytage@gmail.com>
2023-06-19 20:52:45 +0100

    Add eventListener to toggle navbar

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+const openNavIcon = document.querySelector(".nav-icon");
+const closeNavIcon = document.querySelector(".close");
+const toggleNavBar = document.querySelector(".close-navbar");
+const navList = document.querySelectorAll(".nav-list");
+
+const handleClick = () => {
+  toggleNavBar.classList.toggle("open-navbar");
+};
+
+openNavIcon.addEventListener("click", handleClick);
+closeNavIcon.addEventListener("click", handleClick);
