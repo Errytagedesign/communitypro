75fc75175bcb192c9a0a465e168d6cbf543aafc1
Errytagedesign <errytage@gmail.com>
2023-06-22 22:40:10 +0100

    Implementing validations

--- assets/js/index.js
+++ assets/js/index.js
@@ -383,7 +383,5 @@ email.addEventListener("click", function () {
 form.addEventListener("submit", (event) => {
   const regexMail = /[A-Z]/;
   if (regexMail.test(email.value)) {
-    event.preventDefault();
-    error.style.display = "block";
   }
 });
